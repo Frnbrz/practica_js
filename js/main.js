aa3fc15711bb5e2cf9913caf04fb2cb1880bccf7
frnbrz <frnbrz@gmail.com>
2025-03-15 16:27:56 +0100

    feat: initialize cart functionality; add cart elements and render methods

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+let productsCart = []
+let cartListElement = document.querySelector('.cart-list')
+const cartTotalElement = document.querySelector('.cart-total')
 const listaProductos = document.querySelector('.products')
+const listPurchaseElement = document.querySelector('.list-purchase')
 
 renderProducts(productos)
+
+renderCart(productsCart)
+
+
